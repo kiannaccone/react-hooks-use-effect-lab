f9dd4c6a5b75278a6d4588f5ab18fffe001ccb00
Katherine Iannaccone <katherineiannaccone@yahoo.com>
2021-10-19 16:18:45 -0600

    commit tree 6f777026fd3b93a05cf302f191dd0551ba800aa9

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,14 +1,34 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Question({ question, onAnswered }) {
-  const [timeRemaining, setTimeRemaining] = useState(10);
+  const [timeRemaining, setTimeRemaining] = useState(2);
+  
 
-  // add useEffect code
+useEffect(handleTimeOut, []);
+
+function handleTimeOut () {
+  setTimeout(createInterval, 1000);
+  return () => console.log("hello");
+
+function createInterval() {
+  setInterval(subtractTime, 1000)
+}
+
+
+function subtractTime() {
+    setTimeRemaining((currentTimeRemaining) => currentTimeRemaining - 1);
+}
 
   function handleAnswer(isCorrect) {
     setTimeRemaining(10);
     onAnswered(isCorrect);
   }
+
+useEffect(() => {
+  if (timeRemaining === 0) {
+    handleAnswer(false)
+  }
+}, [timeRemaining]);
 
   const { id, prompt, answers, correctIndex } = question;
 
